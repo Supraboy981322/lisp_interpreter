4955908a294754df9a1326fd1de1d39ee29026d6
keeper of the interwebs <97927259+Supraboy981322@users.noreply.github.com>
2026-03-21 16:28:13 -0500

    update test

--- foo.lisp
+++ foo.lisp
@@ -1,6 +1,9 @@
-(stdout "\e[33mcar\bt\e[0m =\t=\nfoo\rb\vfoo\a\n\"\v\r\n")
-(run "(stdout \"bar\n\")")
-(stderr "foo" "\n")
+;(stdout "\e[33mcar\bt\e[0m =\t=\nfoo\rb\vfoo\a\n\"\v\r\n")
+;(run "(stdout \"bar\n\")")
+;(stderr "\npoo" "\rf\n")
+;(stderr "\nfarts" "\rp\n")
+;(stderr "\nbar" "\bt\n" "\r")
+(stdout (< 1 0))
 #|
  TODO: evaluate conditions
  (stdout (? (< 1 0) "is true" "is false"))
