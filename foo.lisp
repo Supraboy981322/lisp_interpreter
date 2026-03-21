c4ec478ac61452a142b1f22e0bfce34d8a52bfd7
keeper of the interwebs <97927259+Supraboy981322@users.noreply.github.com>
2026-03-20 22:16:14 -0500

    TODO

--- foo.lisp
+++ foo.lisp
@@ -1,4 +1,5 @@
 (stdout "\e[33mcar\bt\e[0m =\t=\nfoo\rb\vfoo\a\n\"\v\r\n")
 (run "(stdout \"bar\n\")")
 (stderr "foo")
-;(stdout (? true (stdout "is true") (stdout "is false")))
+; TODO: evaluate conditions
+; (stdout (? true (stdout "is true") (stdout "is false")))
