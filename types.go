cf50e3634217a8b5792e0c0d9d837547db99691a
keeper of the interwebs <97927259+Supraboy981322@users.noreply.github.com>
2026-03-21 16:27:54 -0500

    helper for basic types

--- types.go
+++ types.go
@@ -1,3 +1,19 @@
 package main
 
 // TODO: should probably put these here
+
+var void = Token {
+	Raw: []byte("[VOID]"),
+	Type: TRUE,
+	Note: VALUE,
+}
+var True = Token {
+	Raw: []byte("[TRUE]"),
+	Type: TRUE,
+	Note: VALUE,
+}
+var False = Token {
+	Raw: []byte("[FALSE]"),
+	Type: FALSE,
+	Note: VALUE,
+}
