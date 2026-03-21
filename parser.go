56bd1ed36eb28c2d5475cad9b869d0fd2442af29
keeper of the interwebs <97927259+Supraboy981322@users.noreply.github.com>
2026-03-20 21:57:11 -0500

    refactor open and closing paren handling

--- parser.go
+++ parser.go
@@ -1,44 +1,41 @@
 package main
 
-import ("os";"fmt")
+import (_"os";"fmt")
 
+func _(){fmt.Print()}
 
 func recurse(code []byte) []Token {
 	var p = P{
 		idx: -1,
 		code: code,
 	}
-	var comment bool
-	loop: for p.next() {
-		if comment && p.cur != '\n' { continue loop } else if comment { comment = false }
+	for p.next() {
 		switch p.cur {
 			case '(': if !p.esc {
 				p.Toks = append(p.Toks, mktok(IGNORE, BOX, nil))
-				p.Toks = append(p.Toks, recurse(p.seek_to(')'))...)
-				p.idx--
+				thing := p.seek_whitespace()
+				note, t := p.match_name(thing)
+				p.Toks = append(p.Toks, mktok(note, t, thing))
 			}
 			case '\\': p.esc = !p.esc
 			case '"': if !p.esc {
-				p.Toks = append(p.Toks, mktok(VALUE, STRING, p.seek_to('"')))
+				p.Toks = append(p.Toks, mktok(VALUE, STRING, p.collapse_str()))
 			}
-		  case ')': p.Toks = append(p.Toks, mktok(IGNORE, EOX, nil))
-			case ';': if !p.esc { comment = true } 
+		  case ')':
+				p.Toks = append(p.Toks, mktok(IGNORE, EOX, nil))
+
+			//skip to newline  TODO: multi-line comments
+			case ';': p.seek_to('\n')
+
+			//might do something with this
 			case '\n', ' ', '\r', '\t':
+
 			default: {
-				c := p.cur
-				thing := append([]byte{c}, p.seek_to(' ')...)
-				go_is_dumb := func(foo TokTypeNote, bar TokType) (TokTypeNote, TokType, []byte) {
-					return foo, bar, thing
-				}
-				p.Toks = append(p.Toks, mktok(go_is_dumb(p.match_name(thing))))
+				thing := append([]byte{p.cur}, p.seek_whitespace()...)
+				note, t := p.match_name(thing)
+				p.Toks = append(p.Toks, mktok(note, t, thing))
 			}
 		}
-	}
-	var invalid []byte
-	for _, tok := range p.Toks { if tok.Type == TokType(INVALID) { invalid = tok.Raw } }
-	if invalid != nil { 
-		fmt.Printf("unexpected token: %s\n", string(invalid))
-		os.Exit(1)
 	}
 	return p.Toks
 }
