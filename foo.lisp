199af7a0160e07a9e346d5db088c30778590f571
keeper of the interwebs <97927259+Supraboy981322@users.noreply.github.com>
2026-03-21 21:16:09 -0500

    update test

--- foo.lisp
+++ foo.lisp
@@ -1,11 +1,13 @@
-;(stdout "\e[33mcar\bt\e[0m =\t=\nfoo\rb\vfoo\a\n\"\v\r\n")
+;(print[out] "\e[33mcar\bt\e[0m =\t=\nfoo\rb\vfoo\a\n\"\v\r\n")
 ;(run "(stdout \"bar\n\")")
-;(stderr "\npoo" "\rf\n")
-;(stderr "\nfarts" "\rp\n")
-;(stderr "\nbar" "\bt\n" "\r")
-(stdout (< 1 0))
+;(print[err] "\npoo" "\rf\n")
+;(print[out] "\nfarts" "\rp\n")
+;(print[err] "\nbar" "\bt\n" "\r")
+
+(print[err] "foo\n")
+
 #|
  TODO: evaluate conditions
- (stdout (? (< 1 0) "is true" "is false"))
+ (print[out] (? (< 1 0) "is true" "is false"))
   #| nested comment |#
 |#
